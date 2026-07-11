6cc9c177f8823e5a02ae79e674cfa1c8510e3970
Kusumitha-Muddam <muddamkusumitha@gmail.com>
2026-07-11 12:09:39 +0530

    Time: 2 ms (96.36%), Space: 48.9 MB (15.12%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] countBits(int n) {
+        int []arr=new int[n+1];
+        for(int i=0;i<=n;i++)
+        {
+            int c=0;
+            int num=i;
+            while(num!=0)
+            {
+                num=num&(num-1);
+                c++;
+            }
+            arr[i]=c;
+        }
+        return arr;
+    }
+}

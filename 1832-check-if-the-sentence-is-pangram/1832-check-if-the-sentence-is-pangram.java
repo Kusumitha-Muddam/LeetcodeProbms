5dc230d079e43d2d4477286641f0f035e901783f
Kusumitha-Muddam <muddamkusumitha@gmail.com>
2026-08-24 16:02:03 +0530

    Time: 3 ms (34.5%), Space: 42.8 MB (83.98%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int freq[]=new  int[26];
+        for(int i=0;i<sentence.length();i++)
+        {
+            char ch=sentence.charAt(i);
+            if(ch>='a' && ch<='z')
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]==0)
+            return false;
+        }
+        return true;
+    }
+}
